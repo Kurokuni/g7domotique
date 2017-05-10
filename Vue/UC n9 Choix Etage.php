9aefcd20134a68e23d37263c5824ee6f0319a05f
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-10 11:54:06 +0200

    Premiere etape: creation des bouton pour chaque etage

---
+++ Vue/UC n9 Choix Etage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+require('../Modele/UC n9 Choix Etage.php');
+?>
+if ($etage_ == 1)
+{
+    <div class="bouton">
+    <p>
+    <a href="#">Bouton</a>
+    </p>
+    </div>
+}
+if ($etage_0 == 1)
+{
+    <div class="bouton">
+    <p>
+    <a href="#">Bouton</a>
+    </p>
+    </div>
+}
+if ($etage_1 == 1)
+{
+    <div class="bouton">
+    <p>
+    <a href="#">Bouton</a>
+    </p>
+    </div>
+}
+if ($etage_2== 1)
+{
+    <div class="bouton">
+    <p>
+    <a href="#">Bouton</a>
+    </p>
+    </div>
+}
+if ($etage_2== 1)
+{
+    <div class="bouton">
+    <p>
+    <a href="#">Bouton</a>
+    </p>
+    </div>
+}
+
+</body>
+</html>
