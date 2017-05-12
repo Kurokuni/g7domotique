201c60446515e2ea8f2285f9f9d1bb10d76bbe9b
Kurokuni <jacques.burie@isep.fr>
2017-05-12 10:22:30 +0200

    fonction test mdp

--- Modele/Connexion.php
+++ Modele/Connexion.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jacqu
- * Date: 10/05/2017
- * Time: 10:53
- */
+require '../Else/connexionDB.php';
+
+
+
+function testConnexion ($pseudo, $mdp)
+{
+    $requ = $conn->prepare("SELECT IDUSER FROM user WHERE pseudo = $pseudo");
+    $requ->execute();
+    $requ2 = $conn->prepare("SELECT IDUSER FROM user WHERE pass_word = $mdp");
+    $requ2->execute();
+    echo '<br>';
+    $data = $requ->fetch();
+    if ($requ = $requ2){
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
