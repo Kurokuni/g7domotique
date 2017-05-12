5446c579126d61b229caf7fdf4b47bc17deb78d9
Kurokuni <jacques.burie@isep.fr>
2017-05-12 12:00:59 +0200

    fonction test mdp

--- Controleur/Connexion.php
+++ Controleur/Connexion.php
@@ -1,8 +1,9 @@
 <?php
 require ('../Modele/Connexion.php')
 if(testConnexion($_POST['nom'], $_POST['mdp'])){
-    require ('url');
+    require ('../Vue/Connexion.php');
 } else {
-
+$erreur=true;
+require ('../Vue/Connexion.php');
 }
 ?>
