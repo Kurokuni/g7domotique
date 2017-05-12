4eba8fed9cd71f51c0a4a071749211f63a625574
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-12 11:37:04 +0200

    Demande des requetes terminé

--- Modele/UC n9 Choix Etage.php
+++ Modele/UC n9 Choix Etage.php
@@ -12,7 +12,7 @@
 
     $etage= $bdd -> query('SELECT `etage_min`, `etage_max` FROM `adress_home` WHERE IDHOME=2'); /* Recupère les étage en fonction de l'identité de la maison*/
     $nbetage = $etage -> fetch();//recuperer la donner de l'objet
-    /*print_r($nbetage);*/
+    //print_r($nbetage);
 ?>
 
 
