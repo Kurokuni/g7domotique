e8a08ec9c47d751cb9185f62f69d30aad7719d92
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-12 09:57:58 +0200

    Modification des requetes car on a mis a jour les tables dans phpMYAdmin

--- Modele/UC n9 Choix Etage.php
+++ Modele/UC n9 Choix Etage.php
@@ -1,23 +1,23 @@
 <?php
     try/* si la BDD envoie une erreur je demande un catch pour préciser ou est l'erreur*/
     {
-        $bdd = new PDO ('mysql:host=localhost; dbname=g7database; charset=ttf8', 'root','',array (PDE ::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION));/*On se connecte à MySQL*/
+        $bdd = new PDO ('mysql:host=localhost; dbname=g7database; charset=ttf8', 'root','');/*On se connecte à MySQL*/
     }
     catch (Exception $e)
     {
         die('Erreur: ' .  $e->getMessage());
-        echo "erreur de base de donnés";
+
     }
 
     /*On récupère le contenu de la table etage*/
 
-    $idetageobjet= $bdd -> query( ' select*from etage where IDHOME'); // id client contenue dans la variable session //
+    $idetageobjet= $bdd -> query( ' select Etage_-1, Etage_0, Etage_1, Etage_2, Etage_3 from adress_home where IDHOME'); /* Recupère les étage en fonction de l'identité de la maison*/
     $etage = $idfetageobjet -> fetch();//recuperer la donner de l'objet
-    $etage_= $idetage ['Etage_-1'];
-    $etage_0 = $idetage ['Etage_0'];
-    $etage_1 = $idetage ['Etage_1'];
-    $etage_2 = $idetage ['Etage_2'];
-    $etage_3 = $idetage ['Etage_3'];
+    $idetage_= $idetage ['Etage_-1'];
+    $idetage_0 = $idetage ['Etage_0'];
+    $idetage_1 = $idetage ['Etage_1'];
+    $idetage_2 = $idetage ['Etage_2'];
+    $idetage_3 = $idetage ['Etage_3'];
 ?>
 
 
