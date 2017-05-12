3521e95399b2e228a7b011a04ca18e69e087bfe9
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-12 11:48:23 +0200

    Utilisation de require pour appeler la BDD

--- Modele/UC n9 Choix Etage.php
+++ Modele/UC n9 Choix Etage.php
@@ -1,18 +1,13 @@
 <?php
-    try/* si la BDD envoie une erreur je demande un catch pour préciser ou est l'erreur*/
-    {
-        $bdd = new PDO ('mysql:host=localhost; dbname=g7database; charset=utf8', 'root','');/*On se connecte à MySQL*/
-    }
-    catch (Exception $e)
-    {
-        die('Erreur: ' .  $e->getMessage());
-    }
 
-    /*On récupère le contenu de la table etage*/
+require('../Else/connexionDB.php');
 
-    $etage= $bdd -> query('SELECT `etage_min`, `etage_max` FROM `adress_home` WHERE IDHOME=2'); /* Recupère les étage en fonction de l'identité de la maison*/
+
+/*On récupère le contenu de la table etage*/
+
+    $etage= $conn -> query('SELECT `etage_min`, `etage_max` FROM `adress_home` WHERE IDHOME=2'); /* Recupère les étage en fonction de l'identité de la maison*/
     $nbetage = $etage -> fetch();//recuperer la donner de l'objet
-    //print_r($nbetage);
+    print_r($nbetage);
 ?>
 
 
