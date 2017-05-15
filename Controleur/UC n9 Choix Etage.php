18639f45a50324f1493bc90f5fd76b8ef7beca4f
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-15 10:35:23 +0200

    J'ai ajouté une boucle for pour les etage negatif

--- Controleur/UC n9 Choix Etage.php
+++ Controleur/UC n9 Choix Etage.php
@@ -3,6 +3,12 @@
 require('../Modele/UC n9 Choix Etage.php');/* on recupere ce que l'on a demander dans le modèle */
 
 
+for ($compteur = 0; $compteur < $nbetage[0]; $compteur++)
+{
+    require('../Vue/UC n9 Choix Etage.php');/* on recupere ce que l'on a demander dans la vue */
+}
+
+
 for ($compteur = 0; $compteur < $nbetage[1]; $compteur++)
 {
     require('../Vue/UC n9 Choix Etage.php');/* on recupere ce que l'on a demander dans la vue */
