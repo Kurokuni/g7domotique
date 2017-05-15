f5d57f8c6fadd0431e64fd688d3c4579479bad70
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-15 15:05:40 +0200

    J'ai de nouveau modifié mon code afin de pouvoir ajouter le numero de l'etage sur les boutons. Pour cela, il a falu ajouter du code php afin d'incerer mes nouvelles variables qui sont dans le controleur.

--- Vue/UC n9 Choix Etage.php
+++ Vue/UC n9 Choix Etage.php
@@ -6,7 +6,11 @@
 </head>
 <body>
 
-<input type="button" value="Etage" onclick="alert('Etage')">
+<?php
+for ($etage = $GLOBALS['min'];$etage <= $GLOBALS['max'];$etage++){ // On créé une boucle qui créé des boutons avec le nom de l'étage dedans
+    echo '<input type="button" value="Etage '.$etage.'" onclick="alert(\'Etage\')">'; // On concatène le numéro de l'étage dans la valeur du bouton
+}
+?>
 
 </body>
 </html>
