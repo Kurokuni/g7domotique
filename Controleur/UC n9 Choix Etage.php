1963e5e1f066c120a30c64fe6913089ba024554d
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-15 10:22:48 +0200

    J'ai deplacé la boucle qui été dans la vue, dans le controleur. Ceci m'evite d'avoir des soucies d'écriture entre le php et le html

--- Controleur/UC n9 Choix Etage.php
+++ Controleur/UC n9 Choix Etage.php
@@ -1,3 +1,10 @@
 <?php
 
+require('../Modele/UC n9 Choix Etage.php');/* on recupere ce que l'on a demander dans le modèle */
 
+
+for ($compteur = 0; $compteur < $nbetage[1]; $compteur++)
+{
+    require('../Vue/UC n9 Choix Etage.php');/* on recupere ce que l'on a demander dans la vue */
+}
+?>
