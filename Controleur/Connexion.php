efbfae1d45a48a6da7d90920a388c630b7b44798
Kurokuni <jacques.burie@isep.fr>
2017-05-16 09:39:54 +0200

    connexiox

--- Controleur/Connexion.php
+++ Controleur/Connexion.php
@@ -1,6 +1,9 @@
 <?php
-require ('../Modele/Connexion.php')
-if(testConnexion($_POST['nom'], $_POST['mdp'])){
+require ('../Modele/Connexion.php');
+$Nom=$_POST['nom'];
+$Mdp=$_POST['mdp'];
+$test=testConnexion($Nom, $Mdp);
+if($test){
     require ('../Vue/Connexion.php');
 } else {
 $erreur=true;
