841d6c2301106c2d29ea13a7333b8e5eff92b06e
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-19 14:48:23 +0200

    Le controleur fait seulement la passerelle entre le modele et la vue

--- Controleur/ChoixAdresses.php
+++ Controleur/ChoixAdresses.php
@@ -2,11 +2,5 @@
 
 require('../Modele/ChoixAdresse.php');/* on recupere ce que l'on a demander dans le modèle */
 
-$GLOBALS['street_num'] = $nbadresse[0]; // on stocke le numéro de l'adresse dans une variable globale accessible dans tous les fichiers
-$GLOBALS['street'] = $nbadresse[1]; // idem avec la rue
-$GLOBALS['city'] = $nbadresse[2]; // idem avec la ville
-$GLOBALS['zipcode'] = $nbadresse[3]; // idem avec le code postale
-$GLOBALS['country'] = $nbadresse[4]; // idem avec le pays
 
-
-
+require('../Vue/ChoixAdresse.php'); // On fait appel à la vue de choix d'étage.
