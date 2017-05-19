6a4f8fd3f2fb78f8c6f22b5ef3576fd658e52c32
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-19 09:45:06 +0200

    Suppression des espaces dans le nom du fichier

--- Modele/UCn9ChoixEtage.php
+++ Modele/UCn9ChoixEtage.php
@@ -7,5 +7,5 @@ require('../Else/connexionDB.php');
 
     $etage= $conn -> query('SELECT `etage_min`, `etage_max` FROM `adress_home` WHERE IDHOME=2'); /* Recupère les étage en fonction de l'identité de la maison*/
     $nbetage = $etage -> fetch();/* On recupere la donner de l'objet*/
-    print_r($nbetage);/* Permet d'afficher les données de la requete*/
+    //print_r($nbetage);/* Permet d'afficher les données de la requete*/
 ?>
