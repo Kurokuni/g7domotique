8cc4f82a40051c67286a33261f8e7d634e7396a8
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-19 14:49:28 +0200

    J'affiche les boutons avec les adresses

--- Vue/ChoixAdresse.php
+++ Vue/ChoixAdresse.php
@@ -7,11 +7,13 @@
 <body>
 
 <?php
-require('../Controleur/ChoixAdresse.php');/* on recupere ce que l'on a demander dans le modèle */
-echo '<input type="button" value="Adresse:'.$nbadresse[0].$nbadresse[1].$nbadresse[2].$nbadresse[3].$nbadresse[4].'" onclick="alert(\'Adresse\')">';//On concatène tous les éléments de l'adresse dans la valeur du bouton
+
+foreach ($nbadresse as $element)
+{
+    echo '<input type="button" value="Adresse '.$element[0]. $element[1]. $element[2]. $element[3]. $element[4].'" onclick="alert(\'Adresse\')">';
+}
+
 ?>
-
-
 
 </body>
 </html>
