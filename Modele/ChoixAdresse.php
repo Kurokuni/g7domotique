bdcf8fe7632852c3089fbb97a4a8a9c1f2db6852
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-19 12:07:25 +0200

    creation de la requete pour recuperer les données des adresses

---
+++ Modele/ChoixAdresse.php
@@ -0,0 +1,13 @@
+<?php
+
+require('../Else/connexionDB.php');
+
+
+/*On récupère le contenu des adresses d'un même utilisateur*/
+
+$adresse= $conn -> query('SELECT `street_num`, `street`, `city`,`zipcode`,`country`  FROM `adress_home` WHERE IDUSER=4'); /* Recupère les données des adresses de l'identité d'un utilisateur*/
+$nbadresse = $adresse -> fetchAll();/* On recupere la donner de l'objet*/
+print_r($nbadresse);/* Permet d'afficher les données de la requete*/
+?>
+
+
