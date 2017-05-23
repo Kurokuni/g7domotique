017176f56032bdb7342ee8eb95e3bd7777880824
Kurokuni <jacques.burie@isep.fr>
2017-05-23 10:05:52 +0200

    ajout de la connexion admin

--- Controleur/Controleur_Connexion.php
+++ Controleur/Controleur_Connexion.php
@@ -4,10 +4,17 @@ $Nom=$_POST['nom'];
 $Mdp=$_POST['mdp'];
 $test=testConnexion($Nom, $Mdp);
 if($test){
-    if ($var3=1){
-        require ('../Vue/VueUserConnecte.php');
-    } elseif ($var3=2) {
-        require('../Vue/VueAdminConnecte.php');
+    $role = role($Nom);
+    echo ($role);
+    switch ($role){
+        case "1":
+            require('../Vue/VueUserConnecte.php');
+            break;
+        case "2":
+            require ('../Vue/VueAdminConnecte.php');
+            break;
+        default:
+            require('../Vue/VueUserConnecte.php');
     }
 } else {
 $erreur=true;
