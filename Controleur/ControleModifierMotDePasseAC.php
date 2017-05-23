d83a9a16cce1bdd6ac000c14c10a1c8f93a2fb4b
Kurokuni <jacques.burie@isep.fr>
2017-05-23 10:30:54 +0200

    début mot de passe

---
+++ Controleur/ControleModifierMotDePasseAC.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jacqu
+ * Date: 23/05/2017
+ * Time: 10:18
+ */
---
+++ Vue/VueModifierMotDePasseAC.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+require ('../Header.php');
+?>
+
+
+
+
+
+
+
+
+<?php
+require('../Footer.php');
+?>
+</body>
+</html>
