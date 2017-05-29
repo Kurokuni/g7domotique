8a8b655dff0e00959c5b76340d4d32e57aedf642
OrianneJoannic <orianne.joannic@isep.fr>
2017-05-29 18:45:02 +0200

    Changement de nom du fichier

--- Vue/ChoixAdresse.php
+++ Vue/ChoixAdresse.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>ChoixAdresse</title>
 </head>
 <body>
 
--- Vue/ChoixEtage.php
+++ Vue/ChoixEtage.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>ChoixEtage</title>
 </head>
 <body>
 
