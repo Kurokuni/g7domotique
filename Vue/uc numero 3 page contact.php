ac26db61ef5fbaae9a1b22af658187b593d0cbd9
benoit <benoitoehmichen4@gmail.com>
2017-05-31 11:50:33 +0200

    PP

---
+++ Vue/uc numero 3 page contact.php
@@ -0,0 +1,55 @@
+
+<html lang="fr">
+<head>
+
+    <link href="../design.css" rel="stylesheet"/>
+    <meta charset="UTF-8">
+    <title>CONTACT</title>
+
+</head>
+
+<body>
+
+<?php
+require('../Header.php');
+?>
+
+<style>
+    body{
+        background-image: url("../Images/Accueil.jpg");
+    }
+</style>
+
+
+<br />
+<img src="../Images/Nouveau_Logo_001.png" alt="Photo du logo" height="30%" width="20%" />
+</p>
+<br/>
+<br/>
+<br/>
+<p>
+    Mail : domisep@isep.fr
+</p>
+
+<p>
+    Téléphone  : 01 39 54 97 45
+</p>
+
+Adresse : 10 rue de Vanves, 92 130 Issy les Moulineaux
+<p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d21009.938298449604!2d2.2839214176992626!3d48.834517024233875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d48.824529!2d2.2798536!4m5!1s0x47e671ce3fa0c01f%3A0x9bd1ac22c478d56e!2sISEP+28+Rue+Notre+Dame+des+Champs!3m2!1d48.8453879!2d2.3281109!5e0!3m2!1sfr!2sfr!4v1496223176430" width="400" height="300" frameborder="0" style="border: 12em;" allowfullscreen></iframe>
+</p>
+<br/>
+
+
+
+
+<!-- sauts de ligne a enlever si possible en modifiant le css du footer -->
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<?php
+require('../Footer.php');
+?>
+</body>
+</html>
+
