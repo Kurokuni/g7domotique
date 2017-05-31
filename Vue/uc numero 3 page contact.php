db45932aaef290a53de903e661cbf966f6309e70
benoitoehmichen <benoitoehmichen4@gmail.com>
2017-05-31 11:56:23 +0200

    Update uc numero 3 page contact.php

--- Vue/uc numero 3 page contact.php
+++ Vue/uc numero 3 page contact.php
@@ -1,46 +1,59 @@
 <?php
 session_start();
 ?>
-<!DOCTYPE html>
+
 <html lang="fr">
 <head>
-    <link href="UcNumero3PageContact.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../design.css" rel="stylesheet"/>
     <meta charset="UTF-8">
     <title>CONTACT</title>
+
 </head>
 
 <body>
 
-<br/>
-<br/>
-<br/>
-<table width="4%" border="4" align="center">
-    <tr>
-        <td><div> Mail : domisep@isep.fr </div></td>
-    </tr>
-</table>
+<?php
+require('../Header.php');
+?>
 
-<br/>
-<br/>
-<br/>
-<table width="4%" border="4" align="center">
-    <tr>
-        <td><div> Téléphone  : 00.00.00.00.00 </div></td>
-    </tr>
-</table>
+<style>
+    body{
+        background-image: url("../Images/Accueil.jpg");
+    }
+</style>
 
+
+<br />
+<img src="../Images/Nouveau_Logo_001.png" alt="Photo du logo" height="30%" width="20%" />
+</p>
 <br/>
 <br/>
 <br/>
-<table width="20%" border="4" align="center">
-    <tr>
-        <td><div> Adresse : <a href="https://www.google.fr/maps/place/10+Rue+de+Vanves,+92170+Issy-les-Moulineaux/@48.8250439,2.2779271,17z/data=!3m1!4b1!4m5!3m4!1s0x47e6707980bd3947:0xd54fb6c5e1933333!8m2!3d48.8250404!4d2.2801211">Domisep</a> </div></td>
-    </tr>
-</table>
-<br/>
-<br/>
+<p>
+    Mail : domisep@isep.fr
+</p>
+
+<p>
+    Téléphone  : 01 39 54 97 45
+</p>
+
+Adresse : 10 rue de Vanves, 92 130 Issy les Moulineaux
+<p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d21009.938298449604!2d2.2839214176992626!3d48.834517024233875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d48.824529!2d2.2798536!4m5!1s0x47e671ce3fa0c01f%3A0x9bd1ac22c478d56e!2sISEP+28+Rue+Notre+Dame+des+Champs!3m2!1d48.8453879!2d2.3281109!5e0!3m2!1sfr!2sfr!4v1496223176430" width="400" height="300" frameborder="0" style="border: 12em;" allowfullscreen></iframe>
+</p>
 <br/>
 
+
+
+
+<!-- sauts de ligne a enlever si possible en modifiant le css du footer -->
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<?php
+require('../Footer.php');
+?>
 </body>
 </html>
+
 
