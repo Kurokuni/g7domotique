ce62633ebe4aa86c0cfe28c63b099b0e62f299ca
EternelPapy <nguyen_thanh96@hotmail.fr>
2017-06-06 09:53:02 +0200

    Add files via upload

---
+++ CSS/Header_Connecte.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>DomIsep</title>
+        <link rel="stylesheet" href="design.css" />
+        <style>
+            img {
+                display: block;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body>
+    <header  class="header">
+        <h1 align="center"><a href="../Vue/Accueil.php">Votre maison connectée partout</a></h1>
+                <div class="header_barre">
+
+                    <!--<nav>
+                        <a href="#">Se Connecter</a>
+                    </nav>  -->
+
+                    <form method="get" action="#">
+                        <input type="search" placeholder="Search!" name="search">
+                    </form>
+
+                </div>
+        <a href="../Vue/Accueil.php"> <img src="../Images/Logo.png" style="width:10%" alt="Logo DomIsep"/> </a>
+    </header>
+    <img align="middle" src ="../Images/Accueil.png" style="width:100%;height:100%;" alt="Photo Accueil"/>
+    </body>
+</html>
---
+++ CSS/Header_Deconnecte.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>DomIsep</title>
+    <link rel="stylesheet" href="design.css" />
+    <style>
+        img {
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+<header  class="header">
+    <h1 align="center"><a href="../Vue/Accueil.php">Votre maison connectée partout</a></h1>
+    <a href="../Vue/Accueil.php"> <img src="../Images/Logo.png" style="width:10%" alt="Logo DomIsep"/> </a>
+
+</header>
+<img align="middle" src ="../Images/Accueil.png" style="width:100%;height:100%;" alt="Photo Accueil"/>
+</body>
+</html>
