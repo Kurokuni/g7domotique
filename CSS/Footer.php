bbdcc3b0f9ed72d1dcac6d8ed522f8813326560f
OrianneJoannic <orianne.joannic@isep.fr>
2017-06-09 09:04:19 +0200

    Modification du CSS

--- CSS/Footer.php
+++ CSS/Footer.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<head>
-        <title>DomIsep</title>
-        <link rel="stylesheet" href="http://localhost/g7domotique-master/CSS/design.css" />
-</head>
-<body>
-
-    <footer class="footer">
+<footer class="footer">
 
         <div class="footer-right">
             <a href="#"> Site de l'entreprise Domisep </a>
@@ -14,11 +7,11 @@
         <div class="footer-left">
 
             <p class="footer-links">
-                <a href="http://localhost/g7domotique-master/Vue/uc numero 3 page contact.html">Nous Contacter</a>
+                <a href="http://localhost/g7domotique/Vue/uc numero 3 page contact.html">Nous Contacter</a>
                 ·
-                <a href="http://localhost/g7domotique-master/Vue/mentionlégale.html">Mention Légale</a>
+                <a href="http://localhost/g7domotique/Vue/mentionlégale.html">Mention Légale</a>
             </p>
             <p align="center">G7Domotique &copy; 2017</p>
         </div>
-    </footer>
-</body>
+</footer>
+
