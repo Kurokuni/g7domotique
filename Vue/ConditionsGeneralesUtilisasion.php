0bddab02df75a315107eb579df213b1847261fa1
OrianneJoannic <orianne.joannic@isep.fr>
2017-06-19 16:00:51 +0200

    Création de la page conditions d'utilisations modifiables par l'administrateur

---
+++ Vue/ConditionsGeneralesUtilisasion.php
@@ -0,0 +1,28 @@
+<?php
+if(!isset($_SESSION['connexion'])){
+    session_start();
+    $_SESSION['connexion']=1;
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>DomIsep</title>
+    <link rel="stylesheet" href="http://localhost/g7domotique/CSS/design.css"/>
+</head>
+<header>
+    <?php include '../CSS/Header_Deconnecte.php';?>
+</header>
+<body>
+<br>
+<br>
+<br>
+<br>
+<div class="Conditions">
+    <?php require('../Controleur/getteurAdmin.php');
+    echo($textConditions)?>
+</div>
+</body>
+<footer><?php include '../CSS/Footer.php';?>
+</footer>
+</html>
