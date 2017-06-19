b831c9da5985c848e88848f97f8d219c0773731e
Kurokuni <jacques.burie@isep.fr>
2017-06-19 10:07:11 +0200

    maj du message

--- Vue/Vue_Message.php
+++ Vue/Vue_Message.php
@@ -10,6 +10,7 @@ if(!isset($_SESSION['connexion'])){
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/design.css" />
     <title>Message</title>
+    <meta http-equiv="refresh" content="5; URL=http://localhost/g7domotique/Vue/Vue_Message.php">
 </head>
 <header>
     <?php include '../CSS/Header_Deconnecte.php';?>
@@ -20,11 +21,12 @@ if(!isset($_SESSION['connexion'])){
     <label><input type="text" name="Message" placeholder="Message"/></label>
     <input type="submit" value="Envoyer"/>
 </form>
+
+
 <?php
+require_once ('../Modele/Modele_Message.php');
 affichermessage();
 ?>
-
-
 </body>
 <footer>
     <?php include '../CSS/Footer.php';?>
