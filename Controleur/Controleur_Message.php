b3717f1065e0dc13d79eb7346ffe7f4f5f2831c5
Kurokuni <jacques.burie@isep.fr>
2017-06-26 09:58:18 +0200

    Le vrais nom pour les message

--- Controleur/Controleur_Message.php
+++ Controleur/Controleur_Message.php
@@ -1,8 +1,7 @@
 <?php
 require ('../Modele/Modele_Message.php');
 $Message = $_POST['Message'];
-$Nom = 'aaa';
-//$Nom = $_SESSION["nom"];
+$Nom = $_SESSION["nom"];
 envoyermessage($Nom,$Message);
 
 require ('../Vue/Vue_Message.php');
